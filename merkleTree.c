78d78485cfdf9ad78a1d0c6ee0dbf207328ae459
alecbetancourt <36958016+alecbetancourt@users.noreply.github.com>
2018-07-11 02:01:32 -0400

    Basic outline for program

---
+++ merkleTree.c
@@ -0,0 +1,16 @@
+//
+// Created by William on 7/11/2018.
+//
+
+#include "merkleTree.h"
+
+int main() {
+    //ask how many data blocks
+    //make method to find tree height n (such that 2^n > |data blocks|)
+    //construct bulk of tree using 'hash nodes' - build head and then add 2^(n-1)-1 hash nodes
+    //make final layer data nodes - add #data blocks nodes based on user input
+    //hash nodes: contain hash and children
+    //data nodes: contain file names and children
+    //write recursive function to compute hash of tree
+    //print tree hash to user
+}
---
+++ merkleTree.h
@@ -0,0 +1,8 @@
+//
+// Created by William on 7/11/2018.
+//
+
+#ifndef PROJECT_MERKLETREE_H
+#define PROJECT_MERKLETREE_H
+
+#endif //PROJECT_MERKLETREE_H
