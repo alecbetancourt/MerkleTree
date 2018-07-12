8a027ee62a597dbb4cf6957310c18d44f23319c0
alecbetancourt <36958016+alecbetancourt@users.noreply.github.com>
2018-07-12 07:16:05 -0400

    Changes to hash algorithm and method to get single string implemented

--- merkleTree.c
+++ merkleTree.c
@@ -1,9 +1,6 @@
-//
-// Created by William on 7/11/2018.
-//
+#include "md5.h"
 
-#include "merkleTree.h"
-
+/*
 int main() {
     //ask how many data blocks
     //make method to find tree height n (such that 2^n > |data blocks|)
@@ -11,6 +8,88 @@ int main() {
     //make final layer data nodes - add #data blocks nodes based on user input
     //hash nodes: contain hash and children
     //data nodes: contain file names and children
-    //write recursive function to compute hash of tree
+    //write recursive function to compute hash of tree, if both children of a node are null, set node to null
+    //if only one child is null, set parent to the hash of the non null child concatenated with a copy of itself
     //print tree hash to user
+
+    char string[] = "string";
+    char *argc = string[1];
+
+
+    if (argc < 2) {
+        printf("usage: %s 'string'\n", argv[0]);
+        return 1;
+    }
+
+    char *msg = argv[1];
+    size_t len = strlen(msg);
+
+    // benchmark
+    // int i;
+    // for (i = 0; i < 1000000; i++) {
+    md5(msg, len);
+    // }
+
+    //var char digest[16] := h0 append h1 append h2 append h3 //(Output is in little-endian)
+    uint8_t *p;
+
+    // display result
+
+    p=(uint8_t *)&h0;
+    printf("%2.2x%2.2x%2.2x%2.2x", p[0], p[1], p[2], p[3], h0);
+
+    p=(uint8_t *)&h1;
+    printf("%2.2x%2.2x%2.2x%2.2x", p[0], p[1], p[2], p[3], h1);
+
+    p=(uint8_t *)&h2;
+    printf("%2.2x%2.2x%2.2x%2.2x", p[0], p[1], p[2], p[3], h2);
+
+    p=(uint8_t *)&h3;
+    printf("%2.2x%2.2x%2.2x%2.2x", p[0], p[1], p[2], p[3], h3);
+    puts("");
+
+    return 0;
+}
+*/
+
+int main() {
+
+    char string[] = "string";
+    printf("\nString being hashed: %s\n\n",string);
+
+    char *msg = string;
+    size_t len = strlen(msg);
+
+    // benchmark
+    // int i;
+    // for (i = 0; i < 1000000; i++) {
+    md5(msg, len);
+    // }
+    
+    char digest[100];
+    //var char digest[16] := h0 append h1 append h2 append h3 //(Output is in little-endian)
+    uint8_t *p;
+
+    // display result
+    p=(uint8_t *)&h0;
+    //printf("%2.2x%2.2x%2.2x%2.2x", p[0], p[1], p[2], p[3], h0);
+    printf("%2.2x%2.2x%2.2x%2.2x", p[0], p[1], p[2], p[3], h0);
+    sprintf(digest + strlen(digest), "%2.2x - %2.2x - %2.2x - %2.2x -", p[0], p[1], p[2], p[3], h0);
+    //sprintf(digest + strlen(digest), "%x", *p);
+
+    p=(uint8_t *)&h1;
+    printf("%2.2x%2.2x%2.2x%2.2x", p[0], p[1], p[2], p[3], h1);
+    sprintf(digest + strlen(digest), "%2.2x - %2.2x - %2.2x - %2.2x -", p[0], p[1], p[2], p[3], h1);
+
+    p=(uint8_t *)&h2;
+    printf("%2.2x%2.2x%2.2x%2.2x", p[0], p[1], p[2], p[3], h2);
+    sprintf(digest + strlen(digest), "%2.2x - %2.2x - %2.2x - %2.2x -", p[0], p[1], p[2], p[3], h2);
+
+    p=(uint8_t *)&h3;
+    printf("%2.2x%2.2x%2.2x%2.2x", p[0], p[1], p[2], p[3], h3);
+    sprintf(digest + strlen(digest), "%2.2x - %2.2x - %2.2x - %2.2x", p[0], p[1], p[2], p[3], h3);
+    puts("");
+
+    printf("%s", digest);
+    return 0;
 }
--- merkleTree.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by William on 7/11/2018.
-//
-
-#ifndef PROJECT_MERKLETREE_H
-#define PROJECT_MERKLETREE_H
-
-#endif //PROJECT_MERKLETREE_H
